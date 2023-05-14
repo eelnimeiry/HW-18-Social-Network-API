744b6737f743c659c1f8ae70da6f11628a6ebd6b
eelnimeiry <Elsadekelnimeriy@gmail.com>
2023-05-14 00:01:19 -0700

    creating routes

--- routes/ApiRoutes/APIRoutes.js
+++ routes/ApiRoutes/APIRoutes.js
@@ -3,6 +3,5 @@ const userRoutes = require("../UserRoutes/UserRoutes");
 const thoughtRoutes = require("../ThoughtRoutes/ThoughtRoutes");
 
 router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
